0c2db8dee972bd6d7b21a42c59e0e8f0bc531ecf
William Dunk <williamdunk@gmail.com>
2019-03-09 15:21:21 +0000

    :sparkles: implemented READ connection to backend

--- src/components/target-list.component.js
+++ src/components/target-list.component.js
@@ -1,10 +1,54 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Target = props => (
+  <tr>
+    <td>{props.target.target_description}</td>
+    <td>{props.target.target_priority}</td>
+    <td>
+      <Link to={"/edit/" + props.target._id}>Edit</Link>
+    </td>
+  </tr>
+);
 
 export default class TargetList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { targets: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/targets/")
+      .then(response => {
+        this.setState({ targets: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  targetList() {
+    return this.state.targets.map(function(currentTarget, i) {
+      return <Target target={currentTarget} key={i} />;
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>Welcome to Target List Component!!</p>
+        <h3>Targets:</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Priority</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>{this.targetList()}</tbody>
+        </table>
       </div>
     );
   }
